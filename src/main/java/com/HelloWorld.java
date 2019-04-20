125d588ffd78819a75835ee69f64976335c21a81
jaydeep apte <jaydeepresources@gmail.com>
2019-04-20 12:00:58 +0530

    Add files via upload

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,21 @@
+package com;
+
+public class HelloWorld {
+
+	String msg;
+
+	public HelloWorld() {
+	}
+
+	public HelloWorld(String msg) {
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
