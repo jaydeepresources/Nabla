0d588a7ac0b7843d876f5e1cde06087f2ee35b98
jaydeep apte <jaydeepresources@gmail.com>
2019-04-27 11:20:22 +0530

    Add files via upload

---
+++ java/com/example/demo/Controller.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.posts.Status;
+
+@RestController
+public class Controller {
+
+	@Autowired
+	private PostsRepository repo;
+
+	@GetMapping("/all")
+	public List<Post> getPosts() {
+
+//		System.out.println(repo.findByPlaceContaining("a"));
+		List<Post> posts = new ArrayList<Post>();
+		Iterable<Post> iterable = repo.findAll();
+		for (Post post : iterable) {
+			posts.add(post);
+		}
+		return posts;
+	}
+
+	@PostMapping("/add")
+	public Status savePost(@RequestBody Post post) {
+		repo.save(post);
+		return new Status(true);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public Status deletePost(@PathVariable Integer id) {
+		repo.deleteById(id);
+		return new Status(true);
+	}
+
+	@PutMapping("/edit")
+	public Status editPost(@RequestBody Post post) {
+		repo.save(post);
+		return new Status(true);
+	}
+
+}
